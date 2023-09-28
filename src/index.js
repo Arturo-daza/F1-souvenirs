eb978f6a53bf24a4badfc1efaedcb3f30001e93e
Arturo-daza <davisdaza70@gmail.com>
2023-09-27 20:28:45 -0500

    Update routes and dependencies

--- src/index.js
+++ src/index.js
@@ -2,19 +2,27 @@ const parser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 3000;
-const animalRoutes = require("./routes/animal");
+const productoRoutes = require("./routes/producto");
 const mongoose = require("mongoose");
+
+
 require("dotenv").config();
 app.use(parser.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
 app.use(parser.json()); // transforma los datos a formato JSON
+
+
 //Gestión de las rutas usando el middleware
-app.use("/api", animalRoutes);
+app.use("/api", productoRoutes);
 app.use(express.json());
+
+
 //Conexión a la base de datos
 mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => console.log("Conexión exitosa"))
     .catch((error) => console.log(error));
+
+
 //Conexión al puerto
 app.listen(port, () => {
     
