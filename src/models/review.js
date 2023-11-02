f749a08a7b5e7c65388cec91a05a330a536b419d
Alejandro León <juan.aleon2001@gmail.com>
2023-11-01 23:54:52 -0500

    Refactor review schema to include timestamps

--- src/models/review.js
+++ src/models/review.js
@@ -25,32 +25,37 @@ const mongoose = require('mongoose');
 *         - comment
 
  */
-const reviewSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // Referencia al modelo de usuario
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // Referencia al modelo de usuario
+      required: true,
     },
     product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product', // Referencia al modelo de producto
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product', // Referencia al modelo de producto
+      required: true,
     },
     rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true,
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
     },
     comment: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     date: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now,
     },
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Review = mongoose.model('Review', reviewSchema);
 
