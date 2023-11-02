834a7e1a565766b96f3fd0a1331906430d597a56
Alejandro León <juan.aleon2001@gmail.com>
2023-11-01 23:53:01 -0500

    new: added category model and routes

---
+++ src/models/category.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+/**
+ * @swagger
+ * components:
+ * schemas:
+ *  Category:
+ *   type: object
+ *  properties:
+ *   name:
+ *   type: string
+ *  description: The name of the category
+ * required:
+ * - name
+ * example:
+ * name: Ropa
+ */
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
