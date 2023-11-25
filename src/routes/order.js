6a2e0ff7482e4499c2e5c5c5d37af473da4bb8e5
Alejandro León <juan.aleon2001@gmail.com>
2023-11-25 18:44:30 -0500

    Refactor order routes and add user authentication

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,34 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const cartSchema = require('../models/cart');
 const orderSchema = require('../models/order');
+const userSchema = require('../models/user');
 const auth = require('./auth-validation');
 
 // Ruta para que los compradores realicen un pedido desde su carrito
-router.post('/cart/order-checkout', auth, async (req, res) => {
-  const { userId } = req.body; // Suponiendo que el usuario actual está autenticado y su ID se pasa en el cuerpo de la solicitud
+router.post('/order-checkout', auth, async (req, res) => {
+  const { user, items, totalAmount } = req.body; // Suponiendo que el usuario actual está autenticado y su ID se pasa en el cuerpo de la solicitud
   try {
-    // Buscar el carrito del usuario
-    const cart = await cartSchema
-      .findOne({ user: userId })
-      .populate('items.product');
-    if (!cart) {
-      return res.status(404).json({ message: 'Carrito no encontrado' });
-    }
-
     // Crear un nuevo pedido
     const order = new orderSchema({
-      user: userId,
-      items: cart.items,
-      totalAmount: cart.items.reduce(
-        (total, item) => total + item.product.precio * item.quantity,
-        0
-      ),
+      user: user,
+      items: items,
+      totalAmount: totalAmount,
     });
 
     // Guardar el pedido y vaciar el carrito
     await order.save();
-    await cart.updateOne({ $set: { items: [] } });
+    res.json(order);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get('/my-orders', auth, async (req, res) => {
+  const user = await userSchema.findById(req.userData.user._id); // Suponiendo que el usuario actual está autenticado y su ID se pasa en el cuerpo de la solicitud
+  try {
+    // Buscar el pedido del usuario
+    const order = await orderSchema.find({ user: user._id }).populate('items.product').sort({ createdAt: -1 });
+    if (!order) {
+      return res.status(200);
+    }
     res.json(order);
   } catch (error) {
     res.status(500).json({ message: error.message });
